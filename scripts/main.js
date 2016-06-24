e5986aa1d53c192f5cbfff5c19d71c7ae7d7c883
mattsluis <Sluismatthew@gmail.com>
2016-06-23 17:42:22 -0700

    init

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+console.log('running');
+
+$(document).ready(function() {
+  $('#projectsm-link').localScroll({
+
+   target:'#projects'
+  });
+  $('#contact-link').localScroll({
+
+   target:'#contact'
+  });
+  $('#home-link').localScroll({
+
+   target:'#home'
+  });
+})
+`
